f80d83894db78c688a8070749655df727912bf15
sonminuk <131163417+sonminuk@users.noreply.github.com>
2024-03-27 12:50:00 +0900

    시계만들기 끝

---
+++ src/Clock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Clock(props) {
+    return (
+        <div>
+            <h1>안녕, 리엑트!</h1>
+            <h2>현재 시간: {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -5,15 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './Hello'
 import Library from './chapter_03/Library';
+import Clock from './Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+setInterval(() => {
+  root.render(
   <React.StrictMode>
-    <App />
-    <Hello toWhat="World"/>
-    <Library/>
+    <Clock/>
   </React.StrictMode>
-);
+  );
+}, 1000)
+
+// App, Library.jsx
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//     <Hello toWhat="World"/>
+//     <Library/>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
