86dcd234038b5daf68f741a841056fcfd4b9d3e3
sonminuk <131163417+sonminuk@users.noreply.github.com>
2024-03-27 11:40:12 +0900

    Book, Library

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './Hello'
+import Library from './Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Hello toWhat="World"/>
+    <Library/>
   </React.StrictMode>
 );
 
